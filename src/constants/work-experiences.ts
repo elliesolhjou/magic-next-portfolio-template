5dc1744ca46a13aef738be892db8f5ec55efa382
Ellie Solhjou <elliesolhjou@gmail.com>
2024-10-11 19:01:39 -0500

    updated work experience

--- src/constants/work-experiences.ts
+++ src/constants/work-experiences.ts
@@ -2,27 +2,36 @@ import { Experience } from '@/types/experience';
 
 export const WORK_EXPERIENCES: Experience[] = [
     {
-        company: 'Redit',
-        role: 'Software Engineer',
-        startDate: '2020-01-01',
-        endDate: '2021-01-01',
-        description: 'Developed various web applications.',
-        logoUrl: 'https://example.com/logo-a.png',
-    },
-    {
-        company: 'Texas A&M University',
-        role: 'Graduate Teaching Assistant',
-        startDate: '2017-01-01',
-        endDate: '2018-05-01',
+        company: 'Firstplanit',
+        role: 'Software Engineer Intern',
+        startDate: 'Jul 2024',
+        endDate: 'Sep 2024',
         description: 'Worked on UI/UX design and implementation.',
-        logoUrl: 'https://example.com/logo-b.png',
+        logoUrl: '/images/FPILogo.jpeg',
     },
     {
         company: 'General Assembly',
         role: 'Software Engineering Fellow',
-        startDate: '2023-10-01',
-        endDate: '2024-01-23',
+        startDate: 'Oct 2023',
+        endDate: 'Jan 2024',
         description: 'Built and maintained server-side applications.',
-        logoUrl: 'https://example.com/logo-c.png',
-    }
+        logoUrl: '/images/GeneralAssemblyLogo.jpeg',
+    },
+    {
+        company: 'Various',
+        role: 'Associate Project Manager',
+        startDate: 'May 2018',
+        endDate: 'May 2023',
+        description: 'Managed construction projects with budgets exceeding $300 million',
+        logoUrl: '/images/RED.jpeg',
+    },
+    {
+        company: 'Texas A&M University',
+        role: 'Graduate Teaching Assistant',
+        startDate: 'Jan 2017',
+        endDate: 'May 2018',
+        description: 'Facilitated learning in Project Management course for over 350 students',
+        logoUrl: '/images/TAMULOGO.jpeg',
+    },
+
 ]
